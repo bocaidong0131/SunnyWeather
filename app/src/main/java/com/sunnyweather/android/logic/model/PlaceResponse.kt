97f0fe60bc1eab0d690bf9e99ae390172160baf5
bocaidong <723711369@qq.com>
2021-03-25 15:52:21 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
@@ -4,4 +4,4 @@ import com.google.gson.annotations.SerializedName
 
 data class PlaceResponse(val status:String,val places:List<Place>)
 data class Place(val name:String,val location:Location,@SerializedName("formatted_address") val address:String)
-data class Location(val lng:Int,val lat:Int)
+data class Location(val lng:String,val lat:String)
--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -6,13 +6,20 @@ import androidx.lifecycle.ViewModel
 import com.sunnyweather.android.logic.Repository
 import com.sunnyweather.android.logic.model.Place
 
-class PlaceViewModel:ViewModel() {
+class PlaceViewModel : ViewModel() {
     private val searchLiveData = MutableLiveData<String>()
     val placeList = ArrayList<Place>()
-    val placeLiveData = Transformations.switchMap(searchLiveData){
+    val placeLiveData = Transformations.switchMap(searchLiveData) {
         Repository.searchPlaces(it)
     }
-    fun searchPlaces(query:String){
+
+    fun searchPlaces(query: String) {
         searchLiveData.value = query
     }
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+
+    fun getSavedPlace() = Repository.getSavedPlace()
+
+    fun isPlaceSaved() = Repository.isPlaceSaved()
 }
